d2f6e21d7622797d7128c88cb100b823f610a7b6
aziemnicki <126728897+aziemnicki@users.noreply.github.com>
2023-12-12 23:36:41 +0100

    Update controller

---
+++ controller_pid.py
@@ -0,0 +1,241 @@
+from tello_msgs.srv import TelloAction
+from tello_msgs.msg import TelloResponse
+from tello_interface.srv import TelloState
+
+import time
+from enum import Enum
+from scipy.spatial.transform import Rotation
+from simple_pid import PID
+import numpy as np
+from threading import Timer
+
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from ros2_aruco_interfaces.msg import ArucoMarkers
+
+from std_msgs.msg import Empty
+from gazebo_msgs.msg import ModelStates
+
+
+class ControllerNode(Node):
+
+    class TelloState(Enum):
+        LANDED = 1
+        TAKINGOFF = 2
+        HOVERING = 3
+        FLYING = 4
+        LANDING = 5
+        NONE = 0
+
+    state = TelloState.LANDED
+    next_state = TelloState.NONE
+    action_done = False
+
+    pos_x = pos_y = pos_z = ori_roll = ori_pitch = ori_yaw = 0.0
+
+    pid_x = PID(0.7, 0.001, 0.25)
+    pid_y = PID(0.7, 0.001, 0.25)
+    pid_z = PID(0.7, 0.001, 0.25)
+    pid_yaw = PID(0.7, 0.0, 0.25)
+
+    index = 0
+    points = [
+    [0.0, 0.0, 0.5,  3.12],
+    [0.0, 1.0, 0.5,  3.12],
+    [-1.0, 1.0, 0.5,  4.69],
+    [-2.0, 1.0, 0.5,  4.69],
+    [-2.0, 0.0, 0.5,  0.05],
+    [-2.0, -1.0, 0.5,  0.05],
+    [-1.0, -1.0, 0.5,  1.57],
+    [0.0, -1.0, 0.5,  3.12],
+    [0.0, 0.0, 0.5,  3.12]
+    ]
+    visited_aruco = []
+    aruco_dict = {
+        0: [[0.0, 1.0, 0.0,  0.0],[-1.0, 0.0, 0.0,  1.57]],
+        1: [[-1.0, 0.0, 0.0,  0],[0.0, -1.0, 0.0,  -4.65]],
+        2: [[0.0, -1.0, 0.0,  0.00],[1.0, 0.0, 0.0,  1.52]],
+        3: [[1.0, 0.0, 0.0,  1.57],[0.0, 1.0, 0.0,  0]]
+        }
+    last_marker = False
+
+    def __init__(self):
+        super().__init__('controller_node')
+        self.tello_controller = self.create_subscription(Empty, '/iisrl/tello_controller', self.main_callback, 10)
+        self.tello_response = self.create_subscription(TelloResponse, '/drone1/tello_response', self.tello_response_callback, 10)
+        self.aruco_sub = self.create_subscription(ArucoMarkers, '/aruco_markers', self.aruco_callback, 10)
+        self.tello_position = self.create_subscription(ModelStates, '/gazebo/model_states', self.position_callback, 10)
+
+        self.tello_service_server = self.create_service(TelloState, '/iisrl/tello_state', self.state_callback)
+        self.tello_service_client = self.create_client(TelloAction, '/drone1/tello_action')
+        self.service_request = TelloAction.Request()
+
+        self.get_logger().info('Node initialized')
+
+    def position_callback(self, msg):
+        idx = msg.name.index('tello_1')
+
+        self.pos_x = msg.pose[idx].position.x
+        self.pos_y = msg.pose[idx].position.y
+        self.pos_z = msg.pose[idx].position.z
+
+        rot = Rotation.from_quat([msg.pose[idx].orientation.x,
+                                  msg.pose[idx].orientation.y,
+                                  msg.pose[idx].orientation.z,
+                                  msg.pose[idx].orientation.w])
+        roll, pitch, yaw = rot.as_euler('xyz')
+
+        self.ori_roll = roll
+        self.ori_pitch = pitch
+        if yaw < -np.pi/4:
+            self.ori_yaw = yaw + 2.25* np.pi
+        else:
+            self.ori_yaw = yaw + 0.25*np.pi
+
+        # self.get_logger().info(f"position x: {self.pos_x}")
+        # self.get_logger().info(f"position y: {self.pos_y}")
+        # self.get_logger().info(f"position z: {self.pos_z}")
+        # self.get_logger().info(f"orientation r: {self.ori_roll}")
+        # self.get_logger().info(f"orientation p: {self.ori_pitch}")
+        # self.get_logger().info(f"orientation y: {self.ori_yaw}")
+
+    def aruco_callback(self, markers):
+        # for marker in markers.marker_ids:
+        #     marker_id = marker
+        #     # z = round(marker_pose.position.z, 3)
+        #     if marker_id not in self.visited_aruco:
+        #         self.visited_aruco.append(marker_id)
+        #         for next_move in self.aruco_dict[marker_id]:
+        #             self.points.append([x + y for x, y in zip(self.points[-1], next_move)])
+        #     if marker_id == 3:
+        #         self.last_marker = True
+        # self.get_logger().info(f'{self.points}')
+        pass
+
+    def state_callback(self, request, response):
+        response.state = str(self.state)
+        response.value = int(self.state.value)
+
+        return response
+
+    def main_callback(self, msg):
+        self.get_logger().info('Node activated')
+        self.action_done = False # False, gdy istnieje misja do wykonania; True, gdy testujemy start i ladowanie
+
+        self.state = self.TelloState.LANDED
+        self.next_state = self.TelloState.TAKINGOFF
+
+        self.controller()
+
+    def controller(self):
+        if self.state == self.TelloState.LANDED and self.next_state == self.TelloState.TAKINGOFF:
+            self.taking_off_func()
+
+        if self.state == self.TelloState.HOVERING:
+            if self.action_done:
+                self.landing_func()
+            else:
+                self.mission_func()
+
+    def tello_response_callback(self, msg):
+        if msg.rc == 1:
+            self.state = self.next_state
+            self.next_state = self.TelloState.NONE
+
+        self.controller()
+
+    def taking_off_func(self):
+        self.state = self.TelloState.TAKINGOFF
+        self.next_state = self.TelloState.HOVERING
+
+        # start drona
+        while not self.tello_service_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Oczekuje na dostepnosc uslugi Tello...")
+
+        self.service_request.cmd = 'takeoff'
+        self.tello_service_client.call_async(self.service_request)
+
+    def flying_func(self):            
+
+        self.state = self.TelloState.FLYING
+        self.next_state = self.TelloState.FLYING
+
+        if self.action_done:
+            self.next_state = self.TelloState.HOVERING
+            self.controller()
+        else:
+            self.mission_func()
+    
+    def mission_func(self):
+        # misja do wykonania
+        while not self.tello_service_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Oczekuje na dostepnosc uslugi Tello...")
+
+        self.pid_x.setpoint, self.pid_y.setpoint, self.pid_z.setpoint, self.pid_yaw.setpoint = self.points[self.index]
+        self.pid_yaw.setpoint += 0.25*np.pi
+        dist = np.sqrt((self.pos_x - self.pid_x.setpoint)**2 + (self.pos_y - self.pid_y.setpoint)**2 + (self.pos_z - self.pid_z.setpoint)**2)
+        angle_diff = abs(self.pid_yaw.setpoint - self.ori_yaw)
+        if dist > 0.05 or angle_diff > 0.018:
+            vel_x_glob = self.pid_x(self.pos_x)
+            vel_y_glob = self.pid_y(self.pos_y)
+            vel_z_glob = self.pid_z(self.pos_z)
+            vel_yaw = self.pid_yaw(self.ori_yaw)
+            
+            temp = self.ori_yaw - 0.25*np.pi
+            
+            vel_x_loc = (vel_x_glob * np.cos(temp)) + (vel_y_glob * np.sin(temp))
+            vel_y_loc = (-vel_x_glob * np.sin(temp)) + (vel_y_glob * np.cos(temp))
+
+            self.get_logger().info(f'distance: {dist}')
+            self.get_logger().info(f"vel x: {vel_x_loc}")
+            self.get_logger().info(f"vel y: {vel_y_loc}")
+            self.get_logger().info(f"vel z: {vel_z_glob}")
+            self.get_logger().info(f"position x: {self.pos_x}")
+            self.get_logger().info(f"position y: {self.pos_y}")
+            self.get_logger().info(f"position z: {self.pos_z}")
+            self.get_logger().info(f"orientation: {self.ori_yaw}")
+
+            self.service_request.cmd = f'rc {vel_x_loc} {vel_y_loc} {vel_z_glob} {vel_yaw}'
+            self.tello_service_client.call_async(self.service_request)
+            Timer(0.1, self.mission_func).start()
+        else:    
+            self.get_logger().info(f'Goal position reached: {self.pos_z}')
+            self.service_request.cmd = f'rc 0 0 0 0.0'
+          
+
+            self.tello_service_client.call_async(self.service_request)
+
+            if self.index < len(self.points)-1:
+                self.index += 1
+            else:
+                self.action_done = True
+                self.index = 0
+
+            self.controller()
+            
+    def landing_func(self):
+        self.state = self.TelloState.LANDING
+        self.next_state = self.TelloState.LANDED
+
+        # ladowanie drona
+        while not self.tello_service_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Oczekuje na dostepnosc uslugi Tello...")
+
+        self.service_request.cmd = 'land'
+        self.tello_service_client.call_async(self.service_request)
+
+
+def main(args=None):
+    rclpy.init()
+
+    cn = ControllerNode()
+
+    rclpy.spin(cn)
+    cn.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
